bf908c8f7b5c92c9e32d2ec48d4e7ee0262d377c
yarden <yarden@yarden.tamir@walla.co.il>
2023-01-05 15:27:18 +0200

    fill database with fake data

--- database/migrations/2023_01_05_115312_create_posts_table.php
+++ database/migrations/2023_01_05_115312_create_posts_table.php
@@ -13,13 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('user_post', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('title')->unique();
+            $table->text('excerpt')->nullable();
+            $table->text('body');
+            $table->integer("min_to_read")->default(1);
+            $table->string('image_path')->nullable();
+            $table->boolean('is_published');
             $table->timestamps();
         });
     }
@@ -31,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 };
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +15,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // $this->call(PostsTableSeeder::class);
+        Post::factory(100)->create();
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
